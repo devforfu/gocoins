51294678039561065095c2a6d7c43f9ee01c3d52
devforfu <developer.z@outlook.com>
2019-02-27 21:56:05 +0500

    Database draft

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+CREATE USER docker;
+CREATE DATABASE docker;
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+\c docker;
+
+CREATE TABLE account (
+  user_id serial PRIMARY KEY,
+  identifier VARCHAR(36) NOT NULL,
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO account (identifier) VALUES
+('first'),
+('second'),
+('third');
+
+GRANT ALL PRIVILEGES on TABLE account TO docker;
