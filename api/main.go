7e60ba404035f629f770356fd55f36f4ba864825
devforfu <developer.z@outlook.com>
2019-02-27 21:10:05 +0500

    Compose draft

--- api/main.go
+++ api/main.go
@@ -1,4 +1,4 @@
-package gocoins
+package main
 
 import (
     "log"
@@ -12,6 +12,7 @@ func main() {
 }
 
 func echo(w http.ResponseWriter, req *http.Request) {
+    log.Printf("connected: %s", req.RemoteAddr)
     if _, err := w.Write([]byte("echo")); err != nil {
         log.Fatalf("error sending response: %s", err)
     }
